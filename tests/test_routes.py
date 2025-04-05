231786f70e9faf25f916ef94f0fd1b36737db0e9
Jef <jdovalov@gmail.com>
2025-04-05 11:32:28 -0400

    Add test for illegal method call

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -242,3 +242,8 @@ class TestAccountService(TestCase):
         self.assertEqual( resp.status_code, status.HTTP_200_OK )
         data = resp.get_json()
         self.assertEqual(len(data),0)
+
+    def test_method_not_allowed(self):
+        """It should not allow illegal method calls"""
+        resp = self.client.put(BASE_URL)
+        self.assertEqual( resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED )
